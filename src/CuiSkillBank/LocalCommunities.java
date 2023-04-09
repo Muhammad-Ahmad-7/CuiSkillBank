ac1c1f0173aa4ac1e223755d28680f94a12403a4
Muhammad Ahmad <126343041+Muhammad-Ahmad-7@users.noreply.github.com>
2023-04-09 07:28:47 -0700

    Add files via upload

---
+++ src/CuiSkillBank/LocalCommunities.java
@@ -0,0 +1,7 @@
+package CuiSkillBank;
+
+public class LocalCommunities {
+    public void getActivity(){
+        System.out.println("This is the Local Community page for the Cui skill Bank");
+    }
+}
